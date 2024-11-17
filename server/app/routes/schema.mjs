72e70d0cde2ac55b2414f1c6ecb618f5f074b55c
bwaklog <aditya.mh@outlook.com>
2024-11-17 11:37:15 +0530

    feat: minified req data, added DELETE stories

--- server/app/routes/schema.mjs
+++ server/app/routes/schema.mjs
@@ -53,40 +53,7 @@ const userLoginSchema = {
     }
 }
 
-/*
-SAMPLE CURL REQUEST
-
-curl -X POST \
-http://localhost:4000/stories \
---json '{
-"author_id": "672bc8f9412bdcefc00bc5fa",
-"author": "bar",
-"content": "This is some sample content",
-"title": "This is a title",
-"draft": false,
-"co_authors": ["foo"]
-}'
-*/
 const newStorySchemaRequest = {
-    // Validate author _id in MongoDB
-    // jwt: {
-    //     notEmpty: true,
-    //     isString: true,
-    //     isLength: {
-    //         options: { min: 10 }
-    //     },
-    //     errorMessage: "Invalid JWT"
-    // },
-    author: {
-        // Validate manually if author name matches in the database
-        // for the given author id
-        notEmpty: true,
-        isString: true,
-        isLength: {
-            options: { min: 3, max: 20 }
-        },
-        errorMessage: "Author must be between 3 and 20 characters"
-    },
     co_authors: {
         optional: true,
         isArray: true,
@@ -126,16 +93,6 @@ const updateStoryRequest = {
         },
         errorMessage: "Invalid story id"
     },
-    author: {
-        // Validate manually if author name matches in the database
-        // for the given author id
-        notEmpty: true,
-        isString: true,
-        isLength: {
-            options: { min: 3, max: 20 }
-        },
-        errorMessage: "Author must be between 3 and 20 characters"
-    },
     co_authors: {
         optional: true,
         isArray: true,
@@ -165,4 +122,15 @@ const updateStoryRequest = {
     },
 }
 
-export default { newUserSchemaRequest, newStorySchemaRequest, userLoginSchema, updateStoryRequest };
+const deleteStoryRequest = {
+    id: {
+        notEmpty: true,
+        isString: true,
+        isLength: {
+            options: { min: 24, max: 24 }
+        },
+        errorMessage: "Invalid story id"
+    }
+}
+
+export default { newUserSchemaRequest, newStorySchemaRequest, userLoginSchema, updateStoryRequest, deleteStoryRequest };
